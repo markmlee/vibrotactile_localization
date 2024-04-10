174e6828791aeb84e6806605ff77549d2931db60
Mark <lmy0510@gmail.com>
2024-04-09 23:37:29 -0400

    fixed bug for handling all 6 mics

--- scripts/AudioMonitor.py
+++ scripts/AudioMonitor.py
@@ -29,17 +29,16 @@ class AudioMonitor:
         self.fs = fs  # Sample rate of the audio data
         self.num_channels = 1 # Number of audio channels
         self.sample_duration = sample_duration # Size of the audio window for processing (in seconds)
+        self.num_mics = len(audio_topics) # Number of microphones
 
         # Ratio of overlap between consecutive samples
         self.sample_overlap_ratio = sample_overlap_ratio #if 0.5, then 50% overlap --> publish freq is 2*sample_duration, if 1.0, then no overlap --> publish freq is same as sample_duration
-        self.rolling_buffer = np.zeros(( int(0), self.num_channels)) # Buffer for audio data before being processed
+        self.rolling_buffers = {topic: np.zeros((1, self.num_channels)) for topic in audio_topics}
 
 
         self.audio_subscribers = []
         for topic in audio_topics:
-            # Create a subscriber for each topic
-            # Note: You should replace String with the actual message type
-            sub = rospy.Subscriber(topic, AudioData, self.audio_callback, queue_size=10)
+            sub = rospy.Subscriber(topic, AudioData, lambda msg, topic=topic: self.audio_callback(msg, topic), queue_size=10)
             self.audio_subscribers.append(sub)
 
         #publishers
@@ -52,33 +51,43 @@ class AudioMonitor:
         # ROS publisher for the contact location
         # self.contact_loc_publisher = rospy.Publisher('/contact_location', String, queue_size=10)
 
-    def audio_callback(self, msg):
+    def audio_callback(self, msg, topic):
         """
         Callback method for processing incoming audio data.
 
         Args:
             msg: The incoming ROS message containing audio data.
         """
-        # print(f"Received audio data")
+        # print(f"topic: {topic} and msg size: {len(msg.data)}")
+
+        # Get the rolling buffer for the current topic
+        rolling_buffer = self.rolling_buffers[topic]
 
         #buffer the incoming data to the desired length
         input_data = np.asarray(msg.data).reshape((-1,self.num_channels))
         # print(f"self.rolling_buffer shape[0]: {self.rolling_buffer.shape[0]} input_data shape: {input_data.shape}")
 
         ## Append the input_data to the rolling buffer
-        self.rolling_buffer = np.concatenate((self.rolling_buffer, input_data))
+        rolling_buffer = np.concatenate((rolling_buffer, input_data))
 
         # Check if the buffer is full
-        if self.rolling_buffer.shape[0] > int(self.fs * self.sample_duration):
-            # If buffer is full, process the data
-            # self.process_data()
+        if rolling_buffer.shape[0] > int(self.fs * self.sample_duration):
             
-            #publish contact event
-            self.pub.publish("contact_event")
+            # CHECK only 1 audio topic to see if full, for processing. 
+            # DON'T want separate processing from all the individual topics
+            print(f"topic {topic} is full")
+            if topic == '/audio0':
+                # self.process_data()
+            
+                #publish contact event
+                self.pub.publish("contact_event")
 
             # Slide the buffer by the overlap ratio
             overlap_samples = int(self.fs * self.sample_duration * self.sample_overlap_ratio)
-            self.rolling_buffer = self.rolling_buffer[overlap_samples:]
+            rolling_buffer = rolling_buffer[overlap_samples:]
+
+        # Update the rolling buffer
+        self.rolling_buffers[topic] = rolling_buffer
 
 
 
@@ -106,13 +115,28 @@ class AudioMonitor:
         # return some_condition(audio_data)
         pass
 
+    def process_data(self):
+        """
+        Process the audio data in the rolling buffer.
+        1. Pack the audio data from rolling buffer
+        2. Transform wav data to mel spectrogram
+        3. visualize the mel spectrogram
+        """
+        
+        
+
+
 if __name__ == '__main__':
     print(f" ------ starting audio monitor ------  ")
     rospy.init_node('audio_monitor')
-    audio_topics = ['/audio0']  # TODO: add additional audio topics if needed
+    audio_topics = ['/audio0', '/audio1', '/audio2', '/audio3', '/audio4', '/audio5']  #add additional audio topics if needed
+
     fs = 44100  # Sample rate of the audio data
-    sample_duration = 0.5  # duration of the audio window for processing
-    sample_overlap_ratio = 0.5  # ratio of overlap between consecutive samples
+    sample_duration = 1.0  # duration of the audio window for processing (in sec)
+
+    #if 0.5, then 50% overlap --> publish freq is 2*sample_duration, 
+    #if 1.0, then no overlap --> publish freq is same as sample_duration
+    sample_overlap_ratio = 1.0  # ratio of overlap between consecutive samples (0 to 1)
 
     audio_monitor = AudioMonitor(audio_topics, fs, sample_duration, sample_overlap_ratio)
     rospy.spin()
